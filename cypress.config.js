b52970c6cad4dec0de38a62eb8bfcf1a777861a8
Teodora Jovcheska <teodora.jovcheska@testdevlab.com>
2025-09-30 18:45:03 +0200

    second lesson

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,16 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://www.saucedemo.com",
+    specPattern: "cypress/e2e/**/*.cy.js",
+    viewportHeight: 1000,
+    viewportWidth: 1000,
+    pageLoadTimeout: 60000,
+    defaultCommandTimeout: 6000,
+    video: true,
+    videosFolder: "cypress/videos",
+    screenshotOnRunFailure: true,
+    screenshotsFolder: "cypress/screenshots",
+    // defaultBrowser: "firefox",
   },
 });
--- cypress/e2e/generalTest.cy.js
+++ cypress/e2e/generalTest.cy.js
@@ -3,29 +3,50 @@ it("should appear in cypress runner", () => {
 });
 
 it("should navigate to saucedemo page", () => {
-    cy.visit("https://www.saucedemo.com/")
+    cy.visit("/")
     cy.url().should("include", "saucedemo.com")
+    cy.get('#root').should('be.visible').should('contain.text', 'Swag Labs')
+    cy.get('.login_logo').should('be.visible').should('have.text', 'Swag Labs', { timeout: 15000 })
+    cy.contains('Swag Labs').should('be.visible')
 })
 
-// function doSomething() {
-//     console.log("Something");
-// }
+it("should login with valid credentials", () => {
+    cy.visit("/")
+    cy.get('[data-test="username"]').should('be.visible').type("standard_user")
+    cy.get('[data-test="password"]').should('be.visible').type("secret_sauce")
+    // cy.get('[data-test="login-button"]').should('be.visible').type("Login")
+    cy.get('[data-test="login-button"]').should('be.visible').click()
+    cy.url().should("include", "inventory.html")
+})
 
-// () => console.log("Something"); // arrow function
+it("should login with valid credentials on mobile", () => {
+    cy.viewport(375, 667)
+    cy.visit("/")
+    cy.get('[data-test="username"]').should('be.visible').type("standard_user")
+    cy.get('[data-test="password"]').should('be.visible').type("secret_sauce")
+    // cy.get('[data-test="login-button"]').should('be.visible').type("Login")
+    cy.get('[data-test="login-button"]').should('be.visible').click()
+    cy.url().should("include", "inventory.html")
+})
 
-// function addition(a, b) {
-//     console.log(a + b);
-//     return a + b;
-// }
+it("should login with invalid credentials", () => {
+    cy.visit("/")
+    cy.get('[data-test="username"]').should('be.visible').type("invalid_username")
+    cy.get('[data-test="password"]').should('be.visible').type("12345")
+    cy.get('[data-test="login-button"]').should('be.visible').click()
+    cy.get('[data-test="error"]').should('be.visible').should(
+        'have.text', "Epic sadface: Username and password do not match any user in this service"
+    )
+    cy.url().should("eq", "https://www.saucedemo.com/")
+})
 
-// (a, b) => { 
-//     console.log(a + b); 
-//     return a + b; 
-// } // arrow function
-
-// function testFunction(name, callback) {
-//     console.log(name)
-//     callback();
-// }
-
-// testFunction("test", addition)
+it("should login with locked out user", () => {
+    cy.visit("/")
+    cy.get('[data-test="username"]').should('be.visible').type("locked_out_user")
+    cy.get('[data-test="password"]').should('be.visible').type("secret_sauce")
+    cy.get('[data-test="login-button"]').should('be.visible').click()
+    cy.get('[data-test="error"]').should('be.visible').should(
+        'have.text', "Epic sadface: Sorry, this user has been locked out."
+    )
+    cy.url().should("eq", "https://www.saucedemo.com/")
+})
